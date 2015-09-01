d9d36518fdddfacb57e9d063aff35d65a8350ec2
gusgrins <gug218@lehigh.edu>
2015-09-01 16:33:43 +0000

    Updated WelcomeClass.Java

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,33 @@
+// Gustavo Grinsteins
+// CSE 002 - section 310 HW01
+// 09/01/2015
+// Welcome java Program
+
+// to compile the program
+// javac WelcomeClass.java
+// run the program
+// java WelcomeClass
+
+// define a class
+public class WelcomeClass {
+    
+// add a main method
+    public static void main(String[] args) {
+
+// print the statements and symbols
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    
+    // in order to compile the program without errors
+    // all the "\" symbols need to be doubled e.g. "\\"
+    
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-G--U--G--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This morning I decided to wake up at 5:50 am to go to the gym.");
+    System.out.println("That is a good way to start the day");
+    }
+}
